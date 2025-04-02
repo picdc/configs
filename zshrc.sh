ac6d72378ef9eb248a721de431540d4565becc17
Pierrick Couderc <pierrick.couderc@nomadic-labs.com>
2025-04-02 16:56:49 +0200

    CachyOS Again

--- zshrc.sh
+++ zshrc.sh
@@ -103,8 +103,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvmbash_completion
 
-export SSH_AUTH_SOCK=/run/user/1000/keyring/ssh
-
 alias grep="rg"
 alias ls="eza"
 alias eth-tezt='dune exec etherlink/tezt/tests/main.exe --'
